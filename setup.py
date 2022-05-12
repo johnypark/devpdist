916350e2c0cebc7819ce63de713b5efc21f7bf87
johnypark <parkjohnyc@gmail.com>
2022-05-11 22:45:53 -0400

    Update setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="devpdist", # Replace with your own username
-    version="dev0.0.1",
+    version="0.0.1",
     author="John Park",
     author_email="parkjohnyc@gmail.com",
     description="devpdist",
