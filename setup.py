ffb90929e954365add91dbbf157ddc0ff36d209d
johnypark <parkjohnyc@gmail.com>
2022-05-11 21:42:29 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mytflib", # Replace with your own username
-    version="0.0.2.0",
+    name="devpdist", # Replace with your own username
+    version="dev0.0.1",
     author="John Park",
     author_email="parkjohnyc@gmail.com",
-    description="My TensorFlow Library",
+    description="devpdist",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/johnypark/mytflib",
+    url="https://github.com/johnypark/devpdist",
     packages=setuptools.find_packages(),
-    install_requires = ['tensorflow',
-                       'opencv-python',
-                       'tensorflow-addons',
-                        'one_cycle_tf @ git+https://github.com/johnypark/one_cycle_scheduler_tf'],
+    install_requires = ['tensorflow'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
